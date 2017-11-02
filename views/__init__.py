962494e301b64ab633a4ed109029184c67d4d6e3
Unknown <traustid@gmail.com>
2017-11-02 09:25:07 +0100

    Bugfix på frigg

--- views/__init__.py
+++ views/__init__.py
@@ -7,7 +7,6 @@ from .persons_views import *  # NOQA
 from .records_persons_views import *  # NOQA
 from .persons_places_views import *  # NOQA
 from .socken_v1_views import *  # NOQA
-from .records_media_views import *  # NOQA
 from .socken_views import *  # NOQA
 from .categories_views import *  # NOQA
 from .harad_views import *  # NOQA
